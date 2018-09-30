e29de126e28de2eee678d69d689f9cf0d0151b5a
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2018-09-30 22:29:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -346,7 +346,7 @@ if ($command == '/menu2') {
                     'body' => 
                     array (
                       'type' => 'box',
-                      'layout' => 'vertical',
+                      'layout' => 'horizontal',
                       'contents' => 
                       array (
                         0 => 
